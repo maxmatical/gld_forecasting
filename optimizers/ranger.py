ee6393eb4179ba381b0d47f87d318bd54da3bd7d
Max Tian <max.tian.94@outlook.com>
2020-01-24 15:44:56 -0500

    Update ranger.py

--- optimizers/ranger.py
+++ optimizers/ranger.py
@@ -2,8 +2,8 @@ import math
 import torch
 from torch.optim.optimizer import Optimizer, required
 import itertools as it
-from lookahead import *
-from radam import * 
+from optimizers.lookahead import *
+from optimizers.radam import * 
 
 def Ranger(params, alpha=0.5, k=6, *args, **kwargs):
      radam = RAdam(params, *args, **kwargs)
